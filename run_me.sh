0d3796659937e7fc7f8b884023bc28b80a24a162
Michał Krowiarz <m.krowiarz@gmail.com>
2021-12-03 16:05:19 +0100

    Update run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -30,7 +30,7 @@ sudo apt install ./google-chrome-stable_current_amd64.deb
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/2.1.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Info for the user
